77e2e609dfad32e8d4086fc50c450e6f04e0fcd6
legoffchristophealexandre@gmail.com <legoffchristophealexandre@gmail.com>
2023-11-30 09:28:57 +0100

    coucou

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,11 +1,12 @@
-// composant fonctionnel best 
+// composant fonctionnel best
 
-import Layout from "./component/layout/layout"
+import Layout from "./component/layout/layout";
 
 const Contact = () => {
-return (
+  return (
     <Layout>
-<h1 className="text-3xl text-blue-400 font-bold"> contact</h1>
-</Layout>
-)}
-export default Contact
+      <h1 className="text-3xl text-blue-400 font-bold"> contact</h1>
+    </Layout>
+  );
+};
+export default Contact;
